844521ba3fc872d739d573143da71ba0bf75a42b
plambre <peter.lambrecht@datasite.com>
2022-12-01 15:04:07 -0600

    day 1

--- src/main/kotlin/org/example/adventofcode/puzzle/Day00.kt
+++ src/main/kotlin/org/example/adventofcode/puzzle/Day00.kt
@@ -2,21 +2,62 @@ package org.example.adventofcode.puzzle
 
 import org.example.adventofcode.util.FileLoader
 
-object Day00 {
-    fun printSolution() {
-        println("Example 1: ${part1("/day00_example.txt")}")
-        println("Solution 1: ${part1("/day00.txt")}")
-        println("Example 2: ${part2("/day00_example.txt")}")
-        println("Solution 2: ${part2("/day00.txt")}")
-    }
 
-    fun part1(filePath: String): String {
-        val stringLines = FileLoader.loadFromFile<String>(filePath)
-        return stringLines[0]
-    }
-
-    fun part2(filePath: String): String {
-        val sampleLines = FileLoader.loadFromFile<String>(filePath)
-        return sampleLines[1]
-    }
+fun main() {
+    println("Example 1: ${part1("/day00_example.txt")}")
+    println("Solution 1: ${part1("/day00.txt")}")
+    println("Example 2: ${part2("/day00_example.txt")}")
+    println("Solution 2: ${part2("/day00.txt")}")
 }
+
+fun part1(filePath: String): Int {
+    val stringLines = FileLoader.loadFromFile<String>(filePath)
+
+    var currentSum = 0
+    var highestCalorie = 0
+
+    for (line in stringLines) {
+        if (line.isEmpty()) {
+            if (currentSum > highestCalorie) {
+                highestCalorie = currentSum
+            }
+                currentSum = 0
+        } else {
+            currentSum += line.toInt()
+        }
+    }
+
+    if (currentSum > highestCalorie) {
+        highestCalorie = currentSum
+    }
+
+    return highestCalorie
+}
+
+fun part2(filePath: String): Int {
+    val stringLines = FileLoader.loadFromFile<String>(filePath)
+
+    var currentSum = 0
+    var list = mutableListOf<Int>()
+
+    for (line in stringLines) {
+        if (line.isEmpty() && currentSum != 0) {
+            list.add(currentSum)
+            currentSum = 0
+        } else {
+            currentSum += line.toInt()
+        }
+    }
+
+    if (currentSum != 0) {
+        list.add(currentSum)
+    }
+
+    list.sortDescending()
+
+    println("highest: ${list.elementAt(0)} + 2nd: ${list.elementAt(1)} + 3rd: ${list.elementAt(2)}")
+    val answer = list.elementAt(0) + list.elementAt(1) + list.elementAt(2)
+
+    return answer
+}
+
